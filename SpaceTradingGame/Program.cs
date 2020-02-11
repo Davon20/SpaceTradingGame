4e65df4cc98d869b6c2064e538300c50dbd8b04e
Davon Albury <davonalbury@outlook.com>
2020-02-11 11:54:28 -0500

    Added davonBranch

--- SpaceTradingGame/Program.cs
+++ SpaceTradingGame/Program.cs
@@ -6,7 +6,7 @@ namespace SpaceTradingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("First Paired Programming Project!");
         }
     }
 }
