c2259ea687ad848108bd1284f8469ccc597c9f74
Pablo Becerra <pablo.d.becerra@outlook.com>
2020-02-19 11:46:37 -0500

    Added Planet inventory

--- SpaceTradingGame/Planets.cs
+++ SpaceTradingGame/Planets.cs
@@ -18,21 +18,16 @@ namespace SpaceTradingGame
         }
         public static void alphaCenturia()//if(tradeMenu(pressed b) -> Buy/Sell Buttons
         {
-            Status status = new Status();
-            int fruit = Status.fruit;
-            int meat = Status.meat;
-            int jewels = Status.jewels;
-            int rifles = Status.rifles;
-            int pistols = Status.pistols;
-            int explosives = Status.explosives;
-            int missiles = Status.missiles;
-            int land = Status.land;
-            int residents = Status.residents;
-            int miningTools = Status.miningTools; 
+            (int, int) coordinates = (183, 147);
 
+            int highGradeFuel = 2000;
+            int land = 1000;
+            int residents = 1500;
+            int rifle = 750;
+            int shipMissles = 3000;
 
             string[] terminalItems = new string[] { "High-Grade Fuel", "alphaCenturia Land", "alphaCenturia Residents", "Rifle", "Ship Missiles" };
-            int[] items = new int[] { fruit, meat, jewels, rifles, pistols, explosives, missiles, land, residents, miningTools };
+            int[] items = new int[] { highGradeFuel, land, residents, rifle, shipMissles };
             
             for (int i = 0; i < items.Length; i++)
             {
@@ -45,10 +40,18 @@ namespace SpaceTradingGame
         {
             (int, int) coordinates = (250, 140);
 
+            int lowGradeFuel = 1000;
+            int land = 500;
+            int residents = 700;
+            int pistol = 250;
+            int rareMetals = 3500;
+
             string[] terminalItems = new string[] { "Low-Grade Fuel", "Tatooine Land", "Tatooine Residents", "Pistol", "Rare Metals" };
-            foreach (string items in terminalItems)
+            int[] items = new int[] { lowGradeFuel, land, residents, pistol, rareMetals };
+            for (int i = 0; i < items.Length; i++)
             {
-                Console.WriteLine(items);
+                Console.Write($"{items[i]}: ");
+                Console.WriteLine(terminalItems[i]);
             }
 
             //If the user selects a product = Product Cost - playerMoney
@@ -79,6 +82,24 @@ namespace SpaceTradingGame
         {
             (int, int) coordinates = (496, 532);
 
+            int highGradeFuel = 2500;
+            int land = 4000;
+            int residents = 3000;
+            int missiles = 5000;
+            int pistol = 750;
+            int rifles = 1500;
+
+
+
+
+            string[] terminalItems = new string[] { "High-Grade Fuel", "September Land", "September Residents", "Missiles", "Pistol", "Rifles" };
+            int[] items = new int[] { highGradeFuel, land, residents, missiles, pistol, rifles };
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.Write($"{items[i]}: ");
+                Console.WriteLine(terminalItems[i]);
+            }
 
             //Needs Terminal Item List
         }
