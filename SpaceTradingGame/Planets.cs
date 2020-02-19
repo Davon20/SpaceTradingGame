a74a7c6946bd179a7468a5cace6ce89db47721e2
Davon Albury <davonalbury@outlook.com>
2020-02-19 09:31:13 -0500

    Mid-Way Through Updating Planet Terminal Inventory

--- SpaceTradingGame/Planets.cs
+++ SpaceTradingGame/Planets.cs
@@ -9,36 +9,56 @@ namespace SpaceTradingGame
     {
         public string name;
 
-        public static void Earth() //if(tradeMenu(pressed b) -> Needs to display bankRoll, Buy/Sell Buttons
+        public static void Earth() //if(tradeMenu(pressed b) -> Buy/Sell Buttons
         {
             Console.WriteLine("Hello");
             // REquires if statement: Status.tradeCounter();
         }
-        public static void alphaCenturia()//if(tradeMenu(pressed b) -> Needs to display bankRoll, Buy/Sell Buttons
+        public static void alphaCenturia()//if(tradeMenu(pressed b) -> Buy/Sell Buttons
+        {
+            string[] terminalItems = new string[] { "High-Grade Fuel", "alphaCenturia Land", "alphaCenturia Residents", "Rifle", "Ship Missiles" };
+            foreach (string items in terminalItems)
+            {
+                Console.WriteLine(items);
+            }
+        }
+        public static void Dathomir()//if(tradeMenu(pressed b) -> Buy/Sell Buttons
+        {
+            string[] terminalItems = new string[] { "High-Grade Fuel", "Dathomir Land", "Dathomir Residents", "Expensive Meat", "Expensive Fruit" };
+            foreach (string items in terminalItems)
+            {
+                Console.WriteLine(items);
+            }
+        }
+        public static void Tatooine()//if(tradeMenu(pressed b) -> Buy/Sell Buttons
+        {
+            string[] terminalItems = new string[] { "Low-Grade Fuel", "Tatooine Land", "Tatooine Residents", "Pistol", "Rare Metals" };
+            foreach (string items in terminalItems)
+            {
+                Console.WriteLine(items);
+            }
+
+            //If the user selects a product = Product Cost - playerMoney
+
+            int userInput = int.Parse(Console.ReadLine()); 
+            if (userInput == 1)
+            {
+                
+            }
+        }
+        public static void Alderan()//if(tradeMenu(pressed b) -> Buy/Sell Buttons
+        {
+            string[] terminalItems = new string[] { "Low-Grade Fuel", "Alderan Land", "Alderan Residents", "", "", };
+            foreach (string items in terminalItems)
+            {
+                Console.WriteLine(items);
+            }
+        }
+        public static void Sentinel()//if(tradeMenu(pressed b) -> Buy/Sell Buttons
         {
             //Needs Terminal Item List
         }
-        public static void DathomirIntro()//if(tradeMenu(pressed b) -> Needs to display bankRoll, Buy/Sell Buttons
-        {
-            //Carrie, Chuy, Transportation Service Guy, Scavenger
-        }
-        public static void Dathomir()//if(tradeMenu(pressed b) -> Needs to display bankRoll, Buy/Sell Buttons
-        {
-            //Needs Terminal Item List
-        }
-        public static void Tatooine()//if(tradeMenu(pressed b) -> Needs to display bankRoll, Buy/Sell Buttons
-        {
-            //Needs Terminal Item List
-        }
-        public static void Alderan()//if(tradeMenu(pressed b) -> Needs to display bankRoll, Buy/Sell Buttons
-        {
-            //Needs Terminal Item List
-        }
-        public static void Sentinel()//if(tradeMenu(pressed b) -> Needs to display bankRoll, Buy/Sell Buttons
-        {
-            //Needs Terminal Item List
-        }
-        public static void September()//if(tradeMenu(pressed b) -> Needs to display bankRoll, Buy/Sell Buttons
+        public static void September()//if(tradeMenu(pressed b) -> Buy/Sell Buttons
         {
             //Needs Terminal Item List
         }
