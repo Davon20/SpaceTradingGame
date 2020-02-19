f4bf108ca4c1e16dddda38e7bd9d9bc7f22b014c
Davon Albury <davonalbury@outlook.com>
2020-02-19 11:46:37 -0500

    Planet Inventory Updated

--- SpaceTradingGame/Planets.cs
+++ SpaceTradingGame/Planets.cs
@@ -51,19 +51,41 @@ namespace SpaceTradingGame
         }
         public static void Alderan()//if(tradeMenu(pressed b) -> Buy/Sell Buttons
         {
+            int lowGradeFuel = 500;
+            int land = 3000;
+            int residents = 2000;
+            int art = 4000;
+            int cigars = 2000; 
+
             (int, int) coordinates = (164, 300);
 
             string[] terminalItems = new string[] { "Low-Grade Fuel", "Alderan Land", "Alderan Residents", "", "", };
-            foreach (string items in terminalItems)
+            int[] items = new int[] { lowGradeFuel, land, residents, art, cigars };
+
+            for (int i = 0; i < items.Length; i++)
             {
-                Console.WriteLine(items);
+                Console.Write($"{items[i]}: ");
+                Console.WriteLine(terminalItems[i]);
             }
         }
         public static void Sentinel()//if(tradeMenu(pressed b) -> Buy/Sell Buttons
         {
-            (int, int) coordinates = (354, 256);
+            int highGradeFuel = 1000;
+            int vitaminWater = 500;
+            int upgradedNavigationSystem = 3000;
+            int virtualGameStation = 4000;
+            int halfThePlanet = 75000; 
+            
+            string[] terminalItems = new string[] { "High-Grade Fuel", "Vitamin Water", "Upgraded Navigation System", "Virtual Game Station", "50% of Planet Land", };
+            int[] items = new int[] { highGradeFuel, vitaminWater, upgradedNavigationSystem, virtualGameStation, halfThePlanet };
 
-            //Needs Terminal Item List
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.Write($"{items[i]}: ");
+                Console.WriteLine(terminalItems[i]);
+            }
+
+            (int, int) coordinates = (354, 256);
         }
         public static void September()//if(tradeMenu(pressed b) -> Buy/Sell Buttons
         {
